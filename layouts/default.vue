617d50ff5c1f925941bbda7a3cdd16caa57d2017
Mick <mickheudre@gmail.com>
2020-05-19 15:29:39 +0200

    Initial landing page

---
+++ layouts/default.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app-container" class="container">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="">
+          <h4 class="title is-4">Duplidocus</h4>
+        </a>
+      </div>
+
+      <div class="navbar-menu">
+        <div class="navbar-end">
+          <a class="navbar-item">
+            <h6 class="subtitle is-6">Guide d'impression?</h6>
+          </a>
+          <a class="navbar-item">
+            <h6 class="subtitle is-6">Evènements</h6>
+          </a>
+          <a class="navbar-item">
+            <h6 class="subtitle is-6">Liens</h6>
+          </a>
+        </div>
+      </div>
+    </nav>
+    <div id="pages" class="containers">
+      <nuxt />
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+#app-container {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+}
+#pages {
+  flex: 1;
+  display: flex;
+}
+.navbar {
+  margin: 0 2em 0 2em;
+  padding: 8px;
+}
+
+.content-item {
+  flex: 1
+}
+
+a.navbar-item:hover {
+    background-color: transparent;
+    text-decoration: underline;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="columns is-centered">
+    <div class="column is-1" />
+    <div class="column">
+      <div class="images">
+        <div class="align-right">
+          <div id="place-container" class="image-color">
+            <img id="place" class="image" src="../assets/102.jpg">
+          </div>
+        </div>
+        <div class="align-left">
+          <div id="machine-container" class="image-color">
+            <img id="machine" class="image" src="../assets/machine.gif">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="column is-1" />
+    <div class="column is-4">
+      <hr class="divider">
+      <h4 class="title">
+        Duplidocus est <br>
+        un collectif <br>
+        d’impressions <br>
+        DIY & libertaires
+      </h4>
+      <hr class="divider">
+      <p>
+        Nous imprimons affiches, brochures, fanzines,<br>
+        tracts, petits ouvrages en risographie. <br>
+        Nous ne sommes pas des professionnel.le.s,<br>
+        les tirages sont des moments de rencontres,<br>
+        d’échanges et de complicité.
+      </p>
+    </div>
+    <div class="column is-1" />
+  </div>
+</template>
+
+<script>
+// export default {
+
+// }
+</script>
+
+<style lang="scss">
+
+.columns {
+  flex: 1;
+  margin: 0;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row-reverse;
+}
+.columns:last-child {
+  margin-bottom: 0;
+}
+
+.divider {
+  width: 150px;
+  margin: 0
+}
+
+.divider:last-of-type {
+  margin-bottom: 2em;
+}
+
+p {
+  line-height: 1.5em;
+}
+
+.images {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+}
+
+.title:not(:last-child){
+  margin: .4em 0;
+}
+
+.image-color {
+  background-color: #00838A;
+  mix-blend-mode: multiply;
+  display: inline-block;
+
+}
+.image {
+  mix-blend-mode: screen;
+}
+
+.align-right {
+  display: flex;
+  justify-content: right;
+}
+#place-container {
+  margin-left: auto;
+  margin-right: 0;
+}
+
+#place {
+  max-width: 300px;
+  height: auto;
+}
+
+#machine-container {
+  margin-top: -20px;
+}
+#machine {
+  max-width: 400px;
+  width: auto;
+  height: auto;
+}
+</style>
